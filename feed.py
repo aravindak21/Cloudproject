29402630595e0ae2c50ffe043c1151a0f162be31
Arvind <offaravind@gmail.com>
2024-03-20 18:36:18 +0000

    removed error in feed

--- feed.py
+++ feed.py
@@ -29,7 +29,7 @@ for item in yaml_data['item']:
     xml_tree.SubElement(item_element, 'itunes:author').text = yaml_data['author']
     xml_tree.SubElement(item_element, 'description').text = item['description']
     xml_tree.SubElement(item_element, 'itunes:duration').text = item['duration']
-    xml_tree.SubElement(item_element, 'pubDate').text = item['publishedgit']
+    xml_tree.SubElement(item_element, 'pubDate').text = item['published']
     xml_tree.SubElement(item_element, 'title').text = item['title']
     enclosure = xml_tree.SubElement(item_element, 'enclosure', {
         'url': link_prefix + item['file'],
